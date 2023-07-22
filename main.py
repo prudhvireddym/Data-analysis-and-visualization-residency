120906f0d7730f1649d2c0defe2fac27a0ff95d0
prudhvireddym <prudhvireddy.m@gmail.com>
2023-07-22 13:40:09 -0500

    added csvs into folders

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import pandas as pd
 from rapidfuzz import fuzz, process
 
 
-data_netflix = pd.read_csv('netflix_original_8810.csv')
-data_imdb = pd.read_csv('netflix_imdb_9958.csv')
+data_netflix = pd.read_csv('csvs/netflix_original_8810.csv')
+data_imdb = pd.read_csv('csvs/netflix_imdb_9958.csv')
 
 def common_titles():
     common_titles = set(data_netflix['title']).intersection(set(data_imdb['title']))
@@ -31,14 +31,14 @@ def find_best_match(title):
 
 # Apply the function to the IMDb data
 data_imdb['best_match'] = data_imdb['title'].apply(find_best_match)
-data_imdb.to_csv('data_imdb_Best_match_column.csv', index=False)
+data_imdb.to_csv('csvs/data_imdb_Best_match_column.csv', index=False)
 
 
 #Add the columns from Netflix data to IMDb data
 for idx,col in enumerate(['country', 'show_id','type']):
     data_imdb[col] = data_imdb['best_match'].map(netflix_dict).apply(lambda x: x[col] if isinstance(x, dict) else None)
 
-data_imdb.to_csv('data_imdb__initial_combined.csv', index=False)
+data_imdb.to_csv('csvs/data_imdb__initial_combined.csv', index=False)
 
 # Replace NaN values in 'certificate' and 'duration' columns
 data_imdb['certificate'] = data_imdb['certificate'].fillna(data_imdb['best_match'].map(netflix_dict).apply(lambda x: x['rating'] if isinstance(x, dict) else None))
@@ -48,20 +48,20 @@ data_imdb['duration'] = data_imdb['duration'].fillna(data_imdb['best_match'].map
 data_imdb = data_imdb.drop(columns=['best_match'])
 
 # Save the result as a CSV file
-data_imdb.to_csv('merged_data.csv', index=False)
+data_imdb.to_csv('csvs/merged_data.csv', index=False)
 
 
 
 def clean_merged(merged_csv):
     # Load the merged data
-    data_merged = pd.read_csv('merged_data.csv')
+    data_merged = pd.read_csv('csvs/merged_data.csv')
 
     # Remove rows where the 'country' column is missing
     data_cleaned = data_merged.dropna(subset=['country'])
 
     # Save the cleaned data to a new CSV file
-    data_cleaned.to_csv('merged_cleaned_country.csv', index=False)
+    data_cleaned.to_csv('csvs/merged_cleaned_country.csv', index=False)
 
 
 
-clean_merged('merged_data.csv')
+clean_merged('csvs/merged_data.csv')
